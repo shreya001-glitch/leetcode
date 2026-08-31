3bef3215bac7abec903ed41fac0c8b729ff1b862
shreya001-glitch <shreyasinghaps001@gmail.com>
2026-08-31 12:47:00 +0530

    Time: 1 ms (99.95%), Space: 93.4 MB (97.59%) - LeetHub

---
+++ 0121-best-time-to-buy-and-sell-stock/0121-best-time-to-buy-and-sell-stock.java
@@ -0,0 +1,16 @@
+class Solution {
+    public int maxProfit(int[] prices) {
+        int min=Integer.MAX_VALUE;
+        int max=-1;
+        int profit=0;
+        for(int i=0;i<prices.length;i++)
+        {
+            if(prices[i]<min){
+                min=prices[i];
+            }
+            profit = Math.max(profit, prices[i] - min);
+           
+        }
+        return profit;
+    }
+}
